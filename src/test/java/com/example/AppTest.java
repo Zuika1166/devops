4844f8303c2cbc6440185800bfe77e8e3537c403
Chernyshev Anton Pavlovich <87823136+Zuika1166@users.noreply.github.com>
2026-02-19 23:27:49 +0300

    -

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,17 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
-  @Test
-  void addWorks() {
-    assertEquals(5, App.add(2, 3));
-  }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 3, 5"
+    })
+    void add_shouldCalculateCorrectly(int a, int b, int expected) {
+        assertEquals(expected, App.add(a, b));
+    }
 }
